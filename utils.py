8609f215086ff98475dcca3f31751775b85034b5
Rheyhan <rheyhanfahry@apps.ipb.ac.id>
2024-05-23 16:43:03 +0700

    halo <3

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def create_submission(predicted, path = "submission.csv"):
     folder_loc = '/'.join([i for i in path.split("/")][:-1])
-    if os.path.exists(folder_loc):
+    if not os.path.exists(folder_loc) and folder_loc != '':
         os.makedirs(folder_loc)
     df = pd.read_excel("Data/Submission_Format.xlsx")
     df["Kelas"] = predicted
